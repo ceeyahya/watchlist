3fbde0cf0d872014900b3733ddadbebdc42bd45f
Yahya <chahineyahya@pm.me>
2022-08-10 20:02:54 +0100

    Initial Commit

---
+++ backend/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("fiber")
+	})
+
+}
+
+func main() {
+	app := fiber.New()
+
+	setupRoutes(app)
+
+	app.Use(cors.New())
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
