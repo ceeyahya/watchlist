89b7f58a92bab3183b22a2ba5be34237b43b8ec3
Yahya <chahineyahya@pm.me>
2022-11-27 14:04:02 +0000

    Added the migration notice

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -14,17 +14,14 @@ import { Navbar } from 'components/Navbar';
 function MyApp({ Component, pageProps }: AppProps) {
 	const [queryClient] = useState(() => new QueryClient());
 	return (
-		<div className='relative'>
+		<div className=''>
 			<UserProvider>
-				<Navbar />
-				<div className='max-w-7xl mx-auto py-4 px-2 sm:px-6 lg:px-8'>
-					<QueryClientProvider client={queryClient}>
-						<Hydrate state={pageProps.dehydratedState}>
-							<Component {...pageProps} />
-						</Hydrate>
-						<ReactQueryDevtools initialIsOpen={false} />
-					</QueryClientProvider>
-				</div>
+				<QueryClientProvider client={queryClient}>
+					<Hydrate state={pageProps.dehydratedState}>
+						<Component {...pageProps} />
+					</Hydrate>
+					<ReactQueryDevtools initialIsOpen={false} />
+				</QueryClientProvider>
 			</UserProvider>
 		</div>
 	);
